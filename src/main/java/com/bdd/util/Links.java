6ccb316e362565460367458919a778e4991bcf28
Prakash Suvarnkar <prakashs@evolvingsols.com>
2016-06-02 11:51:15 +0530

    Cucumber Testg_Config file location

--- src/main/java/com/bdd/util/Links.java
+++ src/main/java/com/bdd/util/Links.java
@@ -2,6 +2,6 @@ package com.bdd.util;
 
 public class Links {
 	
-	public static final String propfilepath = "D:\\Cucumber\\Cucumber_Selenium\\src\\main\\java\\com\\bdd\\property\\Config.properties";
+	public static final String propfilepath = "src\\main\\java\\com\\bdd\\property\\Config.properties";
 
 }
